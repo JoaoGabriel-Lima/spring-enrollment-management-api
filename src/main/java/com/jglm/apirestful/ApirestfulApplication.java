292a645ec4f152bf7b7a31a7b1fc522f7dbab3cf
João Marinho <jg.limamarinho202@gmail.com>
2025-10-22 22:46:17 -0300

    Adicionar mais disciplinas, professores e alunos; atualizar inscrições para novas turmas

--- src/main/java/com/jglm/apirestful/ApirestfulApplication.java
+++ src/main/java/com/jglm/apirestful/ApirestfulApplication.java
@@ -53,6 +53,31 @@ public class ApirestfulApplication implements CommandLineRunner {
                 disc3.setCargaHoraria(80);
                 disciplinaRepository.save(disc3);
 
+                Disciplina disc4 = new Disciplina();
+                disc4.setNome("Estruturas de Dados");
+                disc4.setCargaHoraria(80);
+                disciplinaRepository.save(disc4);
+
+                Disciplina disc5 = new Disciplina();
+                disc5.setNome("Sistemas Operacionais");
+                disc5.setCargaHoraria(60);
+                disciplinaRepository.save(disc5);
+
+                Disciplina disc6 = new Disciplina();
+                disc6.setNome("Banco de Dados");
+                disc6.setCargaHoraria(60);
+                disciplinaRepository.save(disc6);
+
+                Disciplina disc7 = new Disciplina();
+                disc7.setNome("Engenharia de Software");
+                disc7.setCargaHoraria(80);
+                disciplinaRepository.save(disc7);
+
+                Disciplina disc8 = new Disciplina();
+                disc8.setNome("Redes de Computadores");
+                disc8.setCargaHoraria(60);
+                disciplinaRepository.save(disc8);
+
                 // Criar professores
                 Professor prof1 = new Professor();
                 prof1.setNome("Carlos Alberto Soares Ribeiro");
@@ -68,6 +93,31 @@ public class ApirestfulApplication implements CommandLineRunner {
                 prof3.setNome("Lucia Maria de Assumpcao Drummond");
                 prof3.setEmail("lucia.drummond@ic.uff.br");
                 professorRepository.save(prof3);
+
+                Professor prof4 = new Professor();
+                prof4.setNome("Maria Cristina Boeres");
+                prof4.setEmail("boeres@ic.uff.br");
+                professorRepository.save(prof4);
+
+                Professor prof5 = new Professor();
+                prof5.setNome("Vinod Rebello");
+                prof5.setEmail("vinod@ic.uff.br");
+                professorRepository.save(prof5);
+
+                Professor prof6 = new Professor();
+                prof6.setNome("Simone Martins");
+                prof6.setEmail("simone@ic.uff.br");
+                professorRepository.save(prof6);
+
+                Professor prof7 = new Professor();
+                prof7.setNome("Isabel Rosseti");
+                prof7.setEmail("isabel.rosseti@ic.uff.br");
+                professorRepository.save(prof7);
+
+                Professor prof8 = new Professor();
+                prof8.setNome("Célio Neves");
+                prof8.setEmail("celio.neves@ic.uff.br");
+                professorRepository.save(prof8);
 
                 // Criar alunos
                 Aluno aluno1 = new Aluno();
@@ -90,6 +140,36 @@ public class ApirestfulApplication implements CommandLineRunner {
                 aluno4.setEmail("thales.abranches@id.uff.br");
                 alunoRepository.save(aluno4);
 
+                Aluno aluno5 = new Aluno();
+                aluno5.setNome("Diogo Serafim");
+                aluno5.setEmail("diogo.serafim@id.uff.br");
+                alunoRepository.save(aluno5);
+
+                Aluno aluno6 = new Aluno();
+                aluno6.setNome("Ana Lívia Barata Ribeiro");
+                aluno6.setEmail("analivia@id.uff.br");
+                alunoRepository.save(aluno6);
+
+                Aluno aluno7 = new Aluno();
+                aluno7.setNome("Larissa Yumi Nakashima");
+                aluno7.setEmail("larissa.nakashima@id.uff.br");
+                alunoRepository.save(aluno7);
+
+                Aluno aluno8 = new Aluno();
+                aluno8.setNome("Luciano Andrade");
+                aluno8.setEmail("luciano.andrade@id.uff.br");
+                alunoRepository.save(aluno8);
+
+                Aluno aluno9 = new Aluno();
+                aluno9.setNome("Gabriel Panza");
+                aluno9.setEmail("gabriel.panza@id.uff.br");
+                alunoRepository.save(aluno9);
+
+                Aluno aluno10 = new Aluno();
+                aluno10.setNome("João Moraes");
+                aluno10.setEmail("joao.moraes@id.uff.br");
+                alunoRepository.save(aluno10);
+
                 // Criar turmas
                 Turma turma1 = new Turma();
                 turma1.setAno("2024");
@@ -111,6 +191,41 @@ public class ApirestfulApplication implements CommandLineRunner {
                 turma3.setProfessor(prof3);
                 turma3.setDisciplina(disc3);
                 turmaRepository.save(turma3);
+
+                Turma turma4 = new Turma();
+                turma4.setAno("2024");
+                turma4.setPeriodo("2024.2");
+                turma4.setProfessor(prof4);
+                turma4.setDisciplina(disc4);
+                turmaRepository.save(turma4);
+
+                Turma turma5 = new Turma();
+                turma5.setAno("2024");
+                turma5.setPeriodo("2024.2");
+                turma5.setProfessor(prof5);
+                turma5.setDisciplina(disc5);
+                turmaRepository.save(turma5);
+
+                Turma turma6 = new Turma();
+                turma6.setAno("2025");
+                turma6.setPeriodo("2025.1");
+                turma6.setProfessor(prof6);
+                turma6.setDisciplina(disc6);
+                turmaRepository.save(turma6);
+
+                Turma turma7 = new Turma();
+                turma7.setAno("2025");
+                turma7.setPeriodo("2025.1");
+                turma7.setProfessor(prof7);
+                turma7.setDisciplina(disc7);
+                turmaRepository.save(turma7);
+
+                Turma turma8 = new Turma();
+                turma8.setAno("2025");
+                turma8.setPeriodo("2025.1");
+                turma8.setProfessor(prof8);
+                turma8.setDisciplina(disc8);
+                turmaRepository.save(turma8);
 
                 // Criar inscrições
                 Inscricao inscricao1 = new Inscricao();
@@ -142,6 +257,101 @@ public class ApirestfulApplication implements CommandLineRunner {
                 inscricao5.setAluno(aluno4);
                 inscricao5.setTurma(turma3);
                 inscricaoRepository.save(inscricao5);
+
+                // Turma 4 - com mais de 5 alunos
+                Inscricao inscricao6 = new Inscricao();
+                inscricao6.setDataHora(LocalDateTime.now().minusDays(5));
+                inscricao6.setAluno(aluno1);
+                inscricao6.setTurma(turma4);
+                inscricaoRepository.save(inscricao6);
+
+                Inscricao inscricao7 = new Inscricao();
+                inscricao7.setDataHora(LocalDateTime.now().minusDays(5));
+                inscricao7.setAluno(aluno2);
+                inscricao7.setTurma(turma4);
+                inscricaoRepository.save(inscricao7);
+
+                Inscricao inscricao8 = new Inscricao();
+                inscricao8.setDataHora(LocalDateTime.now().minusDays(4));
+                inscricao8.setAluno(aluno5);
+                inscricao8.setTurma(turma4);
+                inscricaoRepository.save(inscricao8);
+
+                Inscricao inscricao9 = new Inscricao();
+                inscricao9.setDataHora(LocalDateTime.now().minusDays(4));
+                inscricao9.setAluno(aluno6);
+                inscricao9.setTurma(turma4);
+                inscricaoRepository.save(inscricao9);
+
+                Inscricao inscricao10 = new Inscricao();
+                inscricao10.setDataHora(LocalDateTime.now().minusDays(3));
+                inscricao10.setAluno(aluno7);
+                inscricao10.setTurma(turma4);
+                inscricaoRepository.save(inscricao10);
+
+                Inscricao inscricao11 = new Inscricao();
+                inscricao11.setDataHora(LocalDateTime.now().minusDays(3));
+                inscricao11.setAluno(aluno8);
+                inscricao11.setTurma(turma4);
+                inscricaoRepository.save(inscricao11);
+
+                // Turma 5
+                Inscricao inscricao12 = new Inscricao();
+                inscricao12.setDataHora(LocalDateTime.now().minusDays(8));
+                inscricao12.setAluno(aluno3);
+                inscricao12.setTurma(turma5);
+                inscricaoRepository.save(inscricao12);
+
+                Inscricao inscricao13 = new Inscricao();
+                inscricao13.setDataHora(LocalDateTime.now().minusDays(7));
+                inscricao13.setAluno(aluno9);
+                inscricao13.setTurma(turma5);
+                inscricaoRepository.save(inscricao13);
+
+                // Turma 6
+                Inscricao inscricao14 = new Inscricao();
+                inscricao14.setDataHora(LocalDateTime.now().minusDays(2));
+                inscricao14.setAluno(aluno4);
+                inscricao14.setTurma(turma6);
+                inscricaoRepository.save(inscricao14);
+
+                Inscricao inscricao15 = new Inscricao();
+                inscricao15.setDataHora(LocalDateTime.now().minusDays(2));
+                inscricao15.setAluno(aluno10);
+                inscricao15.setTurma(turma6);
+                inscricaoRepository.save(inscricao15);
+
+                // Turma 7
+                Inscricao inscricao16 = new Inscricao();
+                inscricao16.setDataHora(LocalDateTime.now().minusDays(1));
+                inscricao16.setAluno(aluno5);
+                inscricao16.setTurma(turma7);
+                inscricaoRepository.save(inscricao16);
+
+                Inscricao inscricao17 = new Inscricao();
+                inscricao17.setDataHora(LocalDateTime.now().minusDays(1));
+                inscricao17.setAluno(aluno6);
+                inscricao17.setTurma(turma7);
+                inscricaoRepository.save(inscricao17);
+
+                // Turma 8
+                Inscricao inscricao18 = new Inscricao();
+                inscricao18.setDataHora(LocalDateTime.now());
+                inscricao18.setAluno(aluno7);
+                inscricao18.setTurma(turma8);
+                inscricaoRepository.save(inscricao18);
+
+                Inscricao inscricao19 = new Inscricao();
+                inscricao19.setDataHora(LocalDateTime.now());
+                inscricao19.setAluno(aluno8);
+                inscricao19.setTurma(turma8);
+                inscricaoRepository.save(inscricao19);
+
+                Inscricao inscricao20 = new Inscricao();
+                inscricao20.setDataHora(LocalDateTime.now());
+                inscricao20.setAluno(aluno9);
+                inscricao20.setTurma(turma8);
+                inscricaoRepository.save(inscricao20);
 
                 System.out.println("Dados de exemplo criados com sucesso!");
                 System.out.println("- " + disciplinaRepository.count() + " disciplinas");
